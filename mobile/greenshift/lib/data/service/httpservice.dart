30f7cca365f99b47730bd522f33d7efaa14bcd9f
taworld-star <sustrisimamora0413@gmail.com>
2026-01-10 02:25:59 +0700

    feat: implement HttpService for API interactions and update dependencies in pubspec.yaml

---
+++ mobile/greenshift/lib/data/service/httpservice.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class HttpService {
+  static const String _ipUrl = "10.16.242.164";
+  final String baseUrl = "http://$_ipUrl:8000/api";
+  final String aiUrl = "http://$_ipUrl:5000";
+
+  // ambil header & token
+  Future<Map<String, String>> _getHeaders() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('auth_token');
+
+    var headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    };
+
+    if (token != null) {
+      headers['Authorization'] = 'Bearer $token';
+    }
+
+    return headers;
+  }
+
+  Future<http.Response> get(String endpoint) async {
+    final url = Uri.parse('$baseUrl$endpoint');
+    final headers = await _getHeaders(); 
+    
+    final response = await http.get(url, headers: headers);
+    
+    log('GET $url : ${response.statusCode}');
+    return response;
+  }
+
+  Future<http.Response> post(String endpoint, Map<String,dynamic> body) async {
+    final url = Uri.parse('$baseUrl$endpoint');
+    final headers = await _getHeaders();
+
+    final response = await http.post(
+      url,
+      headers: headers,
+      body: jsonEncode(body),
+    );
+
+    log('POST $url: ${response.statusCode}');
+    return response;
+  }
+
+  Future<http.Response> postAuth(String endpoint, Map<String, dynamic> body) async {
+    final url = Uri.parse('$baseUrl$endpoint');
+    final response = await http.post(
+      url,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(body),
+    );
+    return response;
+  }
+
+  Future<http.Response> postWithFile(String endpoint, Map<String, String> fields, File? file,
+  String fileName, {
+  bool isPython = false,
+  }
+  ) async {
+    try{
+      final base = isPython ? aiUrl : baseUrl;
+      final url = Uri.parse('$base$endpoint');
+
+      final request = http.MultipartRequest('POST', url);
+    }
+  }
+}
