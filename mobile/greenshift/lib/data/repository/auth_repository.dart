639a3dc1cb8136329b3182d8af4d6cf0063d9d6b
taworld-star <sustrisimamora0413@gmail.com>
2026-01-10 13:28:54 +0700

    add AuthRepository for user authentication and token management

---
+++ mobile/greenshift/lib/data/repository/auth_repository.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../service/httpservice.dart';
+import '../model/user_model.dart';
+
+class AuthRepository {
+  final HttpService _httpService = HttpService();
+
+  // LOGIN USER
+  Future<UserResponse?> login(String email, String password) async {
+    try {
+      final response = await _httpService.post('/login', {
+        'email': email,
+        'password': password,
+      });
+
+      if (response.statusCode == 200) {
+        // Mengubah JSON Mentah -> Jadi Object UserResponse
+        final data = UserResponse.fromJson(response.body);
+        
+        //Menyimpan token
+        final jsonMap = jsonDecode(response.body);
+        if (jsonMap['token'] != null) {
+            await _saveToken(jsonMap['token']);
+        }
+        
+        return data;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print("Error Login: $e");
+      return null;
+    }
+  }
+
+  // Menyimpan token ke memori hp 
+  Future<void> _saveToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('auth_token', token);
+  }
+}
