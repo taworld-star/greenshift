4f162a3a950610aac1fb7f109a0095a0afbaae2d
taworld-star <sustrisimamora0413@gmail.com>
2026-01-10 11:27:33 +0700

    HttpService with file upload and CRUD operations

--- mobile/greenshift/lib/data/service/httpservice.dart
+++ mobile/greenshift/lib/data/service/httpservice.dart
@@ -5,6 +5,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HttpService {
+  //Konfigurasi IP
   static const String _ipUrl = "10.16.242.164";
   final String baseUrl = "http://$_ipUrl:8000/api";
   final String aiUrl = "http://$_ipUrl:5000";
@@ -26,6 +27,7 @@ class HttpService {
     return headers;
   }
 
+  //CRUD
   Future<http.Response> get(String endpoint) async {
     final url = Uri.parse('$baseUrl$endpoint');
     final headers = await _getHeaders(); 
@@ -73,6 +75,59 @@ class HttpService {
       final url = Uri.parse('$base$endpoint');
 
       final request = http.MultipartRequest('POST', url);
+
+      final prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString('auth_token');
+
+      request.headers['Accept'] = 'application/json';
+      if (token != null) {
+        request.headers['Authorization'] = 'Bearer $token';
+      }
+
+      request.fields.addAll(fields);
+
+      if(file != null) {
+        final imageFile = await http.MultipartFile.fromPath(
+          fileName,
+          file.path,
+        );
+        request.files.add(imageFile);
+      }
+      log('POST FILE to: $url');
+
+      final streamedResponse = await request.send();
+      final response = await http.Response.fromStream(streamedResponse);
+
+      log('Response: ${response.statusCode} - ${response.body}');
+      return response;
+    } catch (e) {
+      log('Error in postWithFile: $e');
+      rethrow;
     }
+  }
+
+  Future<http.Response> put(String endpoint, Map<String, dynamic> body) async {
+    final url = Uri.parse('$baseUrl$endpoint');
+    final headers = await _getHeaders();
+
+    final response = await http.put(
+      url,
+      headers: headers,
+      body: jsonEncode(body),
+    );
+
+    log('PUT $url : ${response.statusCode}');
+    return response;
+  }
+
+  Future<http.Response> delete(String endpoint) async {
+    final url = Uri.parse('$baseUrl$endpoint');
+    final headers = await _getHeaders();
+
+    final response = await http.delete(url, headers: headers);
+
+    log('DELETE $url : ${response.statusCode}');
+    return response;
+
   }
 }
