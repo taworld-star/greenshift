68ffd92bfefac74ac05f9f7fe3aa50e497e40872
taworld-star <sustrisimamora0413@gmail.com>
2026-01-10 12:24:17 +0700

    add UserResponse and User models for handling user data and API responses

---
+++ mobile/greenshift/lib/data/model/user_model.dart
@@ -0,0 +1,92 @@
+import 'package:meta/meta.dart';
+import 'dart:convert';
+
+class UserResponse {
+    final bool success;
+    final User user;
+
+    UserResponse({
+        required this.success,
+        required this.user,
+    });
+
+    UserResponse copyWith({
+        bool? success,
+        User? user,
+    }) => 
+        UserResponse(
+            success: success ?? this.success,
+            user: user ?? this.user,
+        );
+
+    factory UserResponse.fromJson(String str) => UserResponse.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory UserResponse.fromMap(Map<String, dynamic> json) => UserResponse(
+        success: json["success"],
+        user: User.fromMap(json["user"]),
+    );
+
+    Map<String, dynamic> toMap() => {
+        "success": success,
+        "user": user.toMap(),
+    };
+}
+
+class User {
+    final int id;
+    final String name;
+    final String email;
+    final String role;
+    final int totalScans;
+    final int points;
+
+    User({
+        required this.id,
+        required this.name,
+        required this.email,
+        required this.role,
+        required this.totalScans,
+        required this.points,
+    });
+
+    User copyWith({
+        int? id,
+        String? name,
+        String? email,
+        String? role,
+        int? totalScans,
+        int? points,
+    }) => 
+        User(
+            id: id ?? this.id,
+            name: name ?? this.name,
+            email: email ?? this.email,
+            role: role ?? this.role,
+            totalScans: totalScans ?? this.totalScans,
+            points: points ?? this.points,
+        );
+
+    factory User.fromJson(String str) => User.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory User.fromMap(Map<String, dynamic> json) => User(
+        id: json["id"] ?? 0,
+        name: json["name"] ?? "Tanpa nama" ,
+        email: json["email"] ?? "",
+        role: json["role"] ?? "User",
+        totalScans: int.tryParse(json["total_scans"].toString()) ?? 0,
+        points: int.tryParse(json["points"].toString()) ?? 0,
+    );
+
+    Map<String, dynamic> toMap() => {
+        "id": id,
+        "name": name,
+        "email": email,
+        "role": role,
+        "total_scans": totalScans,
+        "points": points,
+    };
+}
